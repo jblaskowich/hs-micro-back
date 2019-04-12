278625325840d8ab7ba230effa73d5a4807511f8
Stephane Beuret <stephane.beuret@data-essential.com>
2019-04-12 13:38:52 +0200

    add reply to send content

--- main.go
+++ main.go
@@ -16,28 +16,37 @@ import (
 )
 
 var (
-	natsURL  = "demo.nats.io"
-	natsPORT = ":4222"
+	natsURL  = "demo.nats.io"           // Can be superseded by env NATSURL
+	natsPort = ":4222"                  // Can be superseded by env NATSPORT
+	natsPost = "zjnO12CgNkHD0IsuGd89zA" // Can be superseded by env NATSPOST
+	natsGet  = "OWM7pKQNbXd7l75l21kOzA" // Can be superseded by env NATSGET
 	database *sql.DB
 )
 
+// Message is the representation of a post
 type Message struct {
+	ID      string
 	Title   string
 	Content string
 	Date    string
 }
 
-func getEvents() {
-	natsChan := os.Getenv("NATSCHAN")
-	if natsChan == "" {
-		natsChan = "zjnO12CgNkHD0IsuGd89zA"
+func watchPost() {
+	if os.Getenv("NATSURL") != "" {
+		natsURL = os.Getenv("NATSURL")
+	}
+	if os.Getenv("NATSPORT") != "" {
+		natsPort = os.Getenv("NATSPORT")
+	}
+	if os.Getenv("NATSCHAN") != "" {
+		natsPost = os.Getenv("NATSPOST")
 	}
 
-	nc, err := nats.Connect(natsURL + natsPORT)
+	nc, err := nats.Connect("nats://" + natsURL + natsPort)
 	if err != nil {
 		log.Println(err.Error())
 	}
-	nc.Subscribe(natsChan, func(m *nats.Msg) {
+	nc.Subscribe(natsPost, func(m *nats.Msg) {
 		//fmt.Printf("Received message: %s\n", string(m.Data))
 		msg := Message{}
 		err := json.Unmarshal(m.Data, &msg)
@@ -69,6 +78,36 @@ func save2Database(m Message) {
 	}
 }
 
+func reqReply() {
+	if os.Getenv("NATSURL") != "" {
+		natsURL = os.Getenv("NATSURL")
+	}
+	if os.Getenv("NATSPORT") != "" {
+		natsPort = os.Getenv("NATSPORT")
+	}
+	if os.Getenv("NATSCHAN") != "" {
+		natsGet = os.Getenv("NATSGET")
+	}
+
+	nc, err := nats.Connect("nats://" + natsURL + natsPort)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	/*
+		We subscribe to Chan natsGet and we are waiting for a new message
+		 the message contains the reference of an inbox, in which we send our content
+	*/
+	nc.Subscribe(natsGet, func(m *nats.Msg) {
+		log.Println("Repl sent on " + m.Reply)
+		err := nc.Publish(string(m.Reply), []byte(`[{"ID": 1, "Title": "hello world", "Content": "blablabla"}]`))
+		if err != nil {
+			log.Println(err.Error())
+		}
+		nc.Flush()
+	})
+}
+
 func main() {
 	dbUser := os.Getenv("DBUSER")
 	if dbUser == "" {
@@ -98,7 +137,8 @@ func main() {
 	}
 	database = db
 
-	getEvents()
+	watchPost()
+	reqReply()
 	port := os.Getenv("HS-MICRO-BACK")
 	if port == "" {
 		port = ":9090"
